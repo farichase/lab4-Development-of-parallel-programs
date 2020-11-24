11977c935b80545fc8fa0a3823c2e0c0c9d1a9c2
farichase <fbazartinova@mail.ru>
2020-11-24 18:55:34 +0300

    Scripted auto-commit on change (2020-11-24 18:55:34) by gitwatch.sh

--- lab4/src/main/java/mylab4/AkkaApp.java
+++ lab4/src/main/java/mylab4/AkkaApp.java
@@ -1,7 +1,9 @@
 package mylab4;
 
+import akka.actor.ActorSystem;
+
 public class AkkaApp {
     public static void main(String[] args){
-        
+        ActorSystem system = ActorSystem.create("akkalab4");
     }
 }
