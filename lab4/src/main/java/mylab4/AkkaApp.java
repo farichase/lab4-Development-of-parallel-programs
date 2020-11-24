bb61db8de0a44213742141ef6f14929cd8738c90
farichase <fbazartinova@mail.ru>
2020-11-24 19:01:54 +0300

    Scripted auto-commit on change (2020-11-24 19:01:54) by gitwatch.sh

--- lab4/src/main/java/mylab4/AkkaApp.java
+++ lab4/src/main/java/mylab4/AkkaApp.java
@@ -2,10 +2,11 @@ package mylab4;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class AkkaApp {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("akkalab4");
-        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
+        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class, system));
     }
 }
