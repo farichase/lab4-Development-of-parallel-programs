35df935facae4ef968ec6cabd67ada37847840a9
farichase <fbazartinova@mail.ru>
2020-11-25 12:07:43 +0300

    Scripted auto-commit on change (2020-11-25 12:07:43) by gitwatch.sh

--- lab4/src/main/java/mylab4/FunctionStore.java
+++ lab4/src/main/java/mylab4/FunctionStore.java
@@ -1,6 +1,20 @@
 package mylab4;
 
-public class FunctionStore {
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.io.Serializable;
+
+public class FunctionStore implements Serializable {
     private final String packageId;
-    private final String 
+    private final String jsScripts;
+    private final String functionName;
+    private final List<Test> tests;
+
+    @JsonCreator
+    public FunctionStore(){
+        this.packageId = packageId;
+        this.jsScripts = jsScripts;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 }
