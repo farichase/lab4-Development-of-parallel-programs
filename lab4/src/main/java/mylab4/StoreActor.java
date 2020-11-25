f536509c6fc67d65e1cbc428b58761dff280972f
farichase <fbazartinova@mail.ru>
2020-11-25 13:45:23 +0300

    Scripted auto-commit on change (2020-11-25 13:45:23) by gitwatch.sh

--- lab4/src/main/java/mylab4/StoreActor.java
+++ lab4/src/main/java/mylab4/StoreActor.java
@@ -2,11 +2,16 @@ package mylab4;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 
 public class StoreActor extends AbstractActor {
     private ActorRef storeActor;
 
+
+    public StoreActor(ActorSystem system) {
+        this.storeActor = system.actorOf()
+    }
     private void funcHandler(StoreMessage jsFunc){
         for (Test test : jsFunc.getTests()) {
 
