126d93d6973520b3655a137c9334b3b78714ad42
farichase <fbazartinova@mail.ru>
2020-11-25 12:29:07 +0300

    Scripted auto-commit on change (2020-11-25 12:29:07) by gitwatch.sh

--- lab4/src/main/java/mylab4/Test.java
+++ lab4/src/main/java/mylab4/Test.java
@@ -1,11 +1,32 @@
 package mylab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
-import java.util.List;
 
 public class Test implements Serializable {
     private final String testName;
     private final String expectedResult;
     private final Integer[] params;
 
+    @JsonCreator
+    public Test(
+        @JsonProperty("testName") String testName,
+        @JsonProperty("expectedResult") String expectedResult,
+        @JsonProperty("params") Integer[] params
+    ) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+    public String getTestName() {
+        return this.testName;
+    }
+    public String getExpectedResult() {
+        return this.expectedResult;
+    }
+    public Integer[] getParams() {
+        return this.params;
+    }
 }
