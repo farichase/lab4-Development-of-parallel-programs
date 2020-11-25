9f061bb54cb33f417d0e54d8b229570019cbd224
farichase <fbazartinova@mail.ru>
2020-11-25 12:19:04 +0300

    Scripted auto-commit on change (2020-11-25 12:19:02) by gitwatch.sh

--- lab4/src/main/java/mylab4/FunctionStore.java
+++ lab4/src/main/java/mylab4/FunctionStore.java
@@ -1,6 +1,7 @@
 package mylab4;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
@@ -11,10 +12,27 @@ public class FunctionStore implements Serializable {
     private final List<Test> tests;
 
     @JsonCreator
-    public FunctionStore(){
+    public FunctionStore(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("jsScripts") String jsScripts,
+            @JsonProperty("functionName") String functionName,
+            @JsonProperty("tests") List<Test> tests
+    ){
         this.packageId = packageId;
         this.jsScripts = jsScripts;
         this.functionName = functionName;
         this.tests = tests;
+    }
+    public String getPackageId() {
+        return this.packageId;
+    }
+    public String jsScripts() {
+        return this.jsScripts;
+    }
+    public String functionName() {
+        return this.functionName;
+    }
+    public List<Test> getTests () {
+        return this.tests;
     }
 }
