a84502960828f174bf3f4afa86365e541d1ab374
farichase <fbazartinova@mail.ru>
2020-11-25 14:02:45 +0300

    Scripted auto-commit on change (2020-11-25 14:02:44) by gitwatch.sh

--- lab4/src/main/java/mylab4/StoreActor.java
+++ lab4/src/main/java/mylab4/StoreActor.java
@@ -2,5 +2,15 @@ package mylab4;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+
 public class StoreActor implements AbstractActor {
+    private Map<Map<String, String>> store = new HashMap<>()
+    @Override
+    public Receive createReceive(){
+        return receiveBuilder()
+                .create()
+                .match(Test.class, test -> add)
+
+    }
 }
