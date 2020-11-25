dfcf97a109fe0ae13f76e5293f7e606461b0ca8d
farichase <fbazartinova@mail.ru>
2020-11-25 12:22:51 +0300

    Scripted auto-commit on change (2020-11-25 12:22:51) by gitwatch.sh

--- lab4/src/main/java/mylab4/Test.java
+++ lab4/src/main/java/mylab4/Test.java
@@ -1,7 +1,11 @@
 package mylab4;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Test implements Serializable {
+    private final String testName;
+    private final String expectedResult;
+    private final List<Integer> params
 
 }
