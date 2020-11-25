0d158414488d5f07bac6f5ac00834921c5763cbd
farichase <fbazartinova@mail.ru>
2020-11-25 12:39:43 +0300

    Scripted auto-commit on change (2020-11-25 12:39:43) by gitwatch.sh

--- lab4/src/main/java/mylab4/StoreActor.java
+++ lab4/src/main/java/mylab4/StoreActor.java
@@ -4,15 +4,15 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 public class StoreActor extends AbstractActor {
-    private void msgHandler(FunctionStore msg){
-        for (Test test : msg.getTests()) {
-            
+    private void funcHandler(FunctionStore jsFunc){
+        for (Test test : jsFunc.getTests()) {
+
         }
     }
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(FunctionStore.class, message -> msgHandler(message))
+                .match(FunctionStore.class, jsFunc -> funcHandler(jsFunc))
 
                 .build();
     }
