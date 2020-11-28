34c8be6fcfc3aa6dfab291248347f6dc8156f9aa
farichase <fbazartinova@mail.ru>
2020-11-28 18:14:48 +0300

    Scripted auto-commit on change (2020-11-28 18:14:48) by gitwatch.sh

--- lab4/src/main/java/mylab4/FunctionResult.java
+++ lab4/src/main/java/mylab4/FunctionResult.java
@@ -1,4 +1,17 @@
 package mylab4;
 
 public class FunctionResult {
+    private String packageID;
+    private String result;
+
+    public FunctionResult(String packageID, String result) {
+        this.packageID = packageID;
+        this.result = result;
+    }
+    public String getPackageID() {
+        return this.packageID;
+    }
+    public String getResult() {
+        return this.result;
+    }
 }
