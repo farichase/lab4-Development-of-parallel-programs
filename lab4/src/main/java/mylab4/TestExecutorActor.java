df9542deb3aecbed40ddddc5f87b08cc63e27785
farichase <fbazartinova@mail.ru>
2020-11-28 18:11:18 +0300

    Scripted auto-commit on change (2020-11-28 18:11:18) by gitwatch.sh

--- lab4/src/main/java/mylab4/TestExecutorActor.java
+++ lab4/src/main/java/mylab4/TestExecutorActor.java
@@ -8,7 +8,9 @@ import javax.script.ScriptEngineManager;
 
 public class TestExecutorActor extends AbstractActor {
     private static final String ENGINE_NAME = "nashorn";
-    
+    private static final boolean FAIL = false;
+    private static final boolean SUCCESS = true;
+
     @Override
     public Receive createReceive(){
         return receiveBuilder()
@@ -17,11 +19,10 @@ public class TestExecutorActor extends AbstractActor {
                     engine.eval(req.getJsScript());
                     Invocable invocable = (Invocable) engine;
                     String res = invocable.invokeFunction(req.getFunctionName(), req.getParams().toArray()).toString();
-                    int answer = "";
-                    if (res.equals(req.getExpectedResult())) answer = "OK";
-                    else answer = "NOTOK"
-
-                    return new ;
+                    boolean answer;
+                    if (res.equals(req.getExpectedResult())) answer = SUCCESS;
+                    else answer = FAIL;
+                    sender().tell();
                 })
     }
 }
