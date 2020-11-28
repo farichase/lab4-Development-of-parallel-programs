9c2201b2aa33285faba2ee3b279bd817c6dd3a6e
farichase <fbazartinova@mail.ru>
2020-11-28 17:17:31 +0300

    Scripted auto-commit on change (2020-11-28 17:17:30) by gitwatch.sh

--- lab4/src/main/java/mylab4/StoreFunction.java
+++ lab4/src/main/java/mylab4/StoreFunction.java
@@ -3,12 +3,14 @@ package mylab4;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StoreFunction implements Serializable {
     private final String packageId;
     private final String jsScripts;
     private final String functionName;
+    private final ArrayList<Test> params;
 
     @JsonCreator
     public StoreFunction(
