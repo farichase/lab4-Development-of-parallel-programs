8098deee52037830b0ea709a74b8ef3c30a33c89
farichase <fbazartinova@mail.ru>
2020-11-28 19:39:19 +0300

    Scripted auto-commit on change (2020-11-28 19:39:19) by gitwatch.sh

--- lab4/src/main/java/mylab4/AkkaApp.java
+++ lab4/src/main/java/mylab4/AkkaApp.java
@@ -1,15 +1,23 @@
 package mylab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
 
 public class AkkaApp {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("akkalab4");
         ActorRef routeActor = system.actorOf(Props.create(RouteActor.class, system));
         final Http http = Http.get(system);
-
+        final AkkaApp app = new AkkaApp();
+        final Materializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow = app.
     }
 }
