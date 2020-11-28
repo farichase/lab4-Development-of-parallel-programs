895e8ae3bfbca035b283a6ee1a2da7dd46e217cc
farichase <fbazartinova@mail.ru>
2020-11-28 20:12:08 +0300

    Scripted auto-commit on change (2020-11-28 20:12:08) by gitwatch.sh

--- lab4/src/main/java/mylab4/RouteActor.java
+++ lab4/src/main/java/mylab4/RouteActor.java
@@ -4,7 +4,6 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.japi.pf.ReceiveBuilder;
 import akka.routing.RoundRobinPool;
 
 public class RouteActor extends AbstractActor {
