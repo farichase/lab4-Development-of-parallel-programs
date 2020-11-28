72048840ce4761a596a12009e1271f8bb306e5f6
farichase <fbazartinova@mail.ru>
2020-11-28 17:27:02 +0300

    Scripted auto-commit on change (2020-11-28 17:27:02) by gitwatch.sh

--- lab4/src/main/java/mylab4/UnitTest.java
+++ lab4/src/main/java/mylab4/UnitTest.java
@@ -1,11 +1,16 @@
 package mylab4;
 
+import java.util.ArrayList;
+
 public class UnitTest {
     public final String packageID;
     public final String jsScripts;
     public final String functionName;
     public final String testName;
     public final String expectedResult;
+    public final ArrayList<Integer> params;
+
+    public 
 
 }
 
