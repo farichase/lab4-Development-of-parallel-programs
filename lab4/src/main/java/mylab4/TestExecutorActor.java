4aecf22a9d38988253dc7fe0a1da20974c664669
farichase <fbazartinova@mail.ru>
2020-11-28 18:00:22 +0300

    Scripted auto-commit on change (2020-11-28 18:00:22) by gitwatch.sh

--- lab4/src/main/java/mylab4/TestExecutorActor.java
+++ lab4/src/main/java/mylab4/TestExecutorActor.java
@@ -2,12 +2,16 @@ package mylab4;
 
 import akka.actor.AbstractActor;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestExecutorActor extends AbstractActor {
     @Override
     public Receive createReceive(){
         return receiveBuilder()
                 .match(UnitTest.class, req -> {
-                    
+                    ScriptEngine engine = new ScriptEngineManager() {
+                    }
                 })
     }
 }
