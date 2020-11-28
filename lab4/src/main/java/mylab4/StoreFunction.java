72ebfc64b2fe10878649d72310a28b369a548570
farichase <fbazartinova@mail.ru>
2020-11-28 17:19:32 +0300

    Scripted auto-commit on change (2020-11-28 17:19:32) by gitwatch.sh

--- lab4/src/main/java/mylab4/StoreFunction.java
+++ lab4/src/main/java/mylab4/StoreFunction.java
@@ -4,23 +4,24 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 public class StoreFunction implements Serializable {
     private final String packageId;
     private final String jsScripts;
     private final String functionName;
-    private final ArrayList<Test> params;
+    private final ArrayList<Test> tests;
 
     @JsonCreator
     public StoreFunction(
             @JsonProperty("packageId") String packageId,
             @JsonProperty("jsScripts") String jsScripts,
-            @JsonProperty("functionName") String functionName
+            @JsonProperty("functionName") String functionName,
+            @JsonProperty("tests") ArrayList<Test> tests
     ){
         this.packageId = packageId;
         this.jsScripts = jsScripts;
         this.functionName = functionName;
+        this.tests = tests;
     }
     public String getPackageId() {
         return this.packageId;
@@ -30,5 +31,8 @@ public class StoreFunction implements Serializable {
     }
     public String getFunctionName() {
         return this.functionName;
+    }
+    public ArrayList<Test> getTests() {
+        return tests;
     }
 }
