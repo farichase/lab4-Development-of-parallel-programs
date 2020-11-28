eff23d81a323255df82f8eb0248cb0dffb0460ae
farichase <fbazartinova@mail.ru>
2020-11-28 16:51:39 +0300

    Scripted auto-commit on change (2020-11-28 16:51:39) by gitwatch.sh

--- lab4/src/main/java/mylab4/TestExecutorActor.java
+++ lab4/src/main/java/mylab4/TestExecutorActor.java
@@ -3,5 +3,8 @@ package mylab4;
 import akka.actor.AbstractActor;
 
 public class TestExecutorActor extends AbstractActor {
-    
+    @Override
+    public Receive receiveBuilder(){
+        
+    }
 }
