6ad7f02203e175e169c978e21394c4a93021bfcc
farichase <fbazartinova@mail.ru>
2020-11-29 19:19:53 +0300

    Scripted auto-commit on change (2020-11-29 19:19:53) by gitwatch.sh

--- lab4/src/main/java/mylab4/OutputResult.java
+++ lab4/src/main/java/mylab4/OutputResult.java
@@ -1,4 +1,19 @@
 package mylab4;
 
+import java.util.ArrayList;
+
 public class OutputResult {
+    private String id;
+    private ArrayList<String> res;
+
+    public OutputResult(String id, ArrayList<String> res){
+        this.id = id;
+        this.res = res;
+    }
+    public String getId() {
+        return id;
+    }
+    public ArrayList<String> getRes() {
+        return res;
+    }
 }
