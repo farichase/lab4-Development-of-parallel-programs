8209ee8630a7d2b4991ecf3f7382d863093aad9c
farichase <fbazartinova@mail.ru>
2020-11-29 17:33:57 +0300

    Scripted auto-commit on change (2020-11-29 17:33:57) by gitwatch.sh

--- lab4/src/main/java/mylab4/TestExecutorActor.java
+++ lab4/src/main/java/mylab4/TestExecutorActor.java
@@ -22,6 +22,7 @@ public class TestExecutorActor extends AbstractActor {
                     String answer;
                     if (res.equals(req.getExpectedResult())) answer = SUCCESS;
                     else answer = FAIL;
+                    System.out.println("Expected result: " + req.getExpectedResult() + "; result: " + res);
                     sender().tell(new FunctionResult(req.getPackageId(), answer), self());
                 })
                 .build();
